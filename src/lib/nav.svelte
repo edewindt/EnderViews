3851ad175ff1862f913979825805a96cc01a1967
edewindt <edewindt27@outlook.com>
2022-11-20 14:24:20 -0600

    Changed styles

--- src/lib/nav.svelte
+++ src/lib/nav.svelte
@@ -17,18 +17,24 @@
 		cursor: pointer;
 		color: aliceblue;
 		padding: 1rem;
+		text-align: center;
+		opacity: 0.6;
 	}
 	.selected {
-		background-color: red;
+		/* background-color: rgba(0, 116, 124, 0.198); */
+		box-shadow: inset 0 0 0.9em rgb(63, 255, 249);
+		color: rgb(173, 255, 252);
+		opacity: 1;
 	}
 	nav {
-		background-color: black;
+		background-color: rgba(0, 0, 0, 0.568);
 		width: 10rem;
 		height: 100%;
 		position: fixed;
 		overflow: hidden;
 		overflow-y: scroll;
 		z-index: 5;
+		margin-left: 2rem;
 	}
 	nav::-webkit-scrollbar {
 		display: none;
